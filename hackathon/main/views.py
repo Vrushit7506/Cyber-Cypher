4f63b3a93d52ffed3df9837c84073aeba5514845
Vrushit7506 <vrushit7506@gmail.com>
2021-09-20 14:43:03 +0530

    Initial Commit

--- hackathon/main/views.py
+++ hackathon/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+# from main.models import __model-name__
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'main/landing.html')
--- hackathon/registration/views.py
+++ hackathon/registration/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+# from registration.models import __model-name__
 
 # Create your views here.
+
+
+def registration(request):
+    return render(request, 'registration/register.html')
